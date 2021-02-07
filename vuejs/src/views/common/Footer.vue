814a3bd0f1931e6012d062bab8c645828147314f
vetrivelelumalai2 <68172178+vetrivelelumalai2@users.noreply.github.com>
2021-02-07 09:06:40 +0530

    Footer.vue

--- vuejs/src/views/common/Footer.vue
+++ vuejs/src/views/common/Footer.vue
@@ -1,4 +1,27 @@
-
+<template>
+  <footer class="footer pb-5">
+    <div class="content has-text-centered">
+      <div v-if="disclaimer" class="footpad footer-disclaimer">
+        <span class="disclaimer-head">Disclaimer:</span> &nbsp; VETRIVEL Our resources are only for learning and communication, not for any commercial use. Please abide by the laws and regulations of your country, and any illegal behavior shall be borne by the user himself.
+      </div>
+      <div v-if="footerLogo" class="footpad footimage">
+        <img width="150" height="75" class="gloryimage" :src="footerLogoLink">
+      </div>
+      <div class="footpad footicons">
+        Github - <a class="icon-tag" href="https://github.com/tks18/gindex-v4">Shan.tk</a> | Support Me!
+      </div>
+      <div v-if="copyright" class="footpad footer-copyright">
+        Copyright&nbsp;<i class="fas fa-copyright"></i> {{ Date.now() | moment("YYYY") }} | &nbsp;{{ sitename }}
+      </div>
+      <div v-if="license" class="footpad footer-policy">
+        Licensed under FrontEnd - <a href="https://github.com/tks18/gindex-v4/blob/dark-mode-0-1/LICENSE" target="_blank"> GPL 3.0 </a> | Backend - <a href="https://github.com/tks18/gindex-v4/blob/dark-mode-0-1/vuejs/LICENSE" target="_blank">MIT</a>
+      </div>
+      <div v-if="codeofconduct" class="footpad footer-code">
+        Please Read our <a href="https://github.com/tks18/gindex-v4/blob/dark-mode-0-1/CODE_OF_CONDUCT.md" target="_blank">Code of Conduct</a> | <a href="https://raw.githubusercontent.com/tks18/gindex-v4/dark-mode-0-1/CONTRIBUTING.md" target="_blank">Community Guidelines</a>
+      </div>
+    </div>
+  </footer>
+</template>
 <script>
 export default {
   props: {},
